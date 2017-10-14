beb5f7ce160609be99a6fe1e9bb8f7095acbd4e8
sreyasramachandran <32782550+sreyasramachandran@users.noreply.github.com>
2017-10-14 23:13:06 +1100

    Initial prices display on UI integrated with reference data.

---
+++ MarketData.UI/Converters/Not.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MarketData.UI.Converters
+{
+    public class Not : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (targetType == typeof(bool))
+            {
+                return !(bool)value;                
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return Convert(value, targetType, parameter, culture);
+        }
+    }
+}
